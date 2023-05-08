fb65a664c332c6c93d7618f5526ef072c4f38e8d
dragon2462 <powerzheng21205@gmail.com>
2023-05-08 00:31:28 -0400

    Zeta8

--- index.js
+++ index.js
@@ -5,3 +5,32 @@ navLinks.forEach((link, index) => {
         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+1}s`;
        }
     });
+    // Smooth scroll effect for internal links
+$(document).on('click', 'a[href^="#"]', function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+  });
+  const mobileMenu = document.querySelector('.mobile-menu');
+const nav = document.querySelector('nav');
+
+mobileMenu.addEventListener('click', () => {
+  nav.classList.toggle('active'); /* toggle the visibility of the navigation menu */
+});
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
